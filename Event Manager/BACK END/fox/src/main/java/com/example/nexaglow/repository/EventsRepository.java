43fa48d34496e27e778c92ed3e4b5ead6d2e1a71
Ujjual <113343711+UjjualA@users.noreply.github.com>
2024-03-06 11:59:13 +0530

    Delete Event Manager/BACK END/fox directory

--- Event Manager/BACK END/fox/src/main/java/com/example/nexaglow/repository/EventsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.nexaglow.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.nexaglow.entity.Events;
-
-public interface EventsRepository extends JpaRepository<Events, Long>{
-    
-}
--- Event Manager/BACK END/fox/src/main/java/com/example/nexaglow/repository/SignupRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.nexaglow.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.nexaglow.entity.Signup;
-
-public interface SignupRepository extends JpaRepository<Signup, Long> {
-
-}
